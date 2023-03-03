56c54b0ade131a5f506be5052fc8732faac3b498
RostyslavMykhats <mirosrik@gmail.com>
2023-03-03 18:00:35 +0200

    gsg

---
+++ js/form.js
@@ -0,0 +1,10 @@
+let name = document.getElementById('name').value,
+    phone = document.getElementById('phone').value,
+    check = document.getElementById('check');
+
+function sendForm () {
+    if (name === '') {
+        
+    }
+} 
+
